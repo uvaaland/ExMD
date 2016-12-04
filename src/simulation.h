73a7d85087003d314c2b2728eb127f55fd6a2ff0
Hugh Wilson <hughwilson1@hotmail.co.uk>
2016-12-04 10:39:06 -0500

    Add bare files for the simulation class

---
+++ src/simulation.h
@@ -0,0 +1,13 @@
+/** @file   simulation.h 
+ *  @brief  Header for the simulation class
+ *
+ *  @author Hugh Wilson (hswilson@princeton.edu)
+ *  @date   2016-12-04
+ *  @bug    No known bugs
+ */ 
+
+#ifndef SRC_SIMULATION_H_
+#define SRC_SIMULATION_H_
+
+
+#endif /* SRC_SIMULATION_H_ */

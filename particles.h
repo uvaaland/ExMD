737ee5c3d8dd5cfc808c866e4f0b002fd69a1526
buralcan <buralcan@princeton.edu>
2016-12-15 00:15:59 -0500

    Add files via upload

---
+++ particles.h
@@ -0,0 +1,17 @@
+#ifndef SRC_PARTICLES_H_
+#define SRC_PARTICLES_H_
+#include <string> // for input
+
+class Particles {
+ public:
+    Particles(std::string filename, int npart = 0);
+    ~Particles();
+    int nparticles;
+    double (*p)[3];
+    double (*v)[3];
+    double *mass;
+    double *radius;
+    void fill(std::string filename);
+};
+
+#endif  // SRC_PARTICLES_H_

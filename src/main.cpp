246fc4049fc9dc195aca89df2c5f50d3da78cbcd
uvaaland <ubvaaland@gmail.com>
2017-01-02 16:07:24 -0500

    Revert "particles updated"

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/** @file   main.cpp
+/** @file   main.cpp 
  *  @brief  Main driving program for ExMD simlation
  *
  *  @author Hugh Wilson (hswilson@princeton.edu)
@@ -19,13 +19,18 @@
 int main() {
   std::cout << "working\n";
 
-#define DIM 3
+  #define DIM 3
 
   /* Make a particles object */
-  std::string filename =
-      "https://github.com/APC524/ExMD/blob/master/test/data.txt";
+  int nparticles = 2;
+  double positions[2][DIM] = {{-10, 0, 0}, {10, 0, 0}};
+  double velocites[2][DIM] = {{1, 0, 0}, {-1, 0, 0}};
+  double masses[DIM] = {1, 1, 1};
+  double radii[DIM] = {1, 1, 1};
+
   Particles *particles;
-  particles = new Particles(filename, 0);
+  particles = new Particles(nparticles, positions, \
+          velocites, masses, radii);
 
   /* Make a physics object */
   Physics *physics;
@@ -36,13 +41,13 @@ int main() {
   int output_period = 1;
 
   Simulation *simulation;
-  simulation = new Simulation(dt, output_period, particles->nparticles, DIM,
-                              particles, physics);
+  simulation = new Simulation(dt, output_period, nparticles, DIM, \
+          particles, physics);
 
   /* Step through time */
   simulation->SetParametersHDF5();
   for (int i = 0; i < 20; i++) {
-    simulation->Step();
+      simulation->Step();
   }
 
   printf("Simulation finished!\n");

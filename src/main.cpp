3b30c07c6790b9a0cd9e6537f7277dc68842bed2
Christy Vaughn <ratherbedanzin@aol.com>
2017-01-09 13:37:49 -0500

    stress testing

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,7 @@
 #include "physics.h"
 #include "force.h"
 #include "gravity.h"
+#include "flocking.h"
 #include "boundary.h"
 #include "write.h"
 
@@ -33,28 +34,32 @@ int main() {
   /* Simulation parameters */
   int nsteps = 100;
   /* Make a particles object */
-  const int nparticles = 4;
-  double positions[nparticles][DIM] = {{2, 0, 0}, {-2, 0, 0}, \
-  {0, 2 , 0}, {0, 6, 0}};
-  double velocites[nparticles][DIM] = {{-1, -1, 0}, {1, -1, 0}, \
-  {0, 0, 0}, {0, 0, 0}};
-  double masses[nparticles] = {1, 1, 1, 1};
-  double radii[nparticles] = {1, 1, 1, 1};
+  const int nparticles = 6;
+  double positions[nparticles][DIM] = {{10, 0, 0}, {-10, 0, 0}, \
+  {0, 10, 0}, {0, 0, 0}, {10, 2, 0}, {-10, 2, 0}};
+  double velocites[nparticles][DIM] = {{-1, 0, 0}, {1, 0, 0}, \
+  {0, -1, 0}, {0, 0, 0}, {-1, 0, 0}, {1, 0, 0}};
+  double masses[nparticles] = {1, 1, 1, 1, 1, 1};
+  double radii[nparticles] = {1, 1, 1, 1, 1, 1};
 
   Particles *particles;
   particles = new Particles(nparticles, positions, \
           velocites, masses, radii);
 
   /* Make force object (depending on user input) UPDATE THIS */
-  double G = 6.67408 * pow(10, -11);  // gravitational constant
+  double G = 0.1; // 6.67408 * pow(10, -11);  // gravitational constant
   Force *gravity = new Gravity(G);
+
+  double beta = 0;
+  Force *flocking = new Flocking(beta);
 
   /* Make a physics object */
   Physics *physics;
   physics = new Physics();
 
   /* Add forces to physics */
-  physics->AddForce(gravity);
+  // physics->AddForce(gravity);
+  // physics->AddForce(flocking);
 
   /* Make a boundary object */
   Boundary boundary = { reflecting, {{-100, 100}, {-100, 100}, {-100, 100}} };

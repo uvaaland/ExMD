a711920e353a27e666b67b03baf634deaa2e4561
uvaaland <ubvaaland@gmail.com>
2017-01-10 15:51:26 -0500

    Add cmake functions for building examples

--- example/example.cmake
+++ example/example.cmake
@@ -1,12 +1,27 @@
+# Function for running the example script
+function(generate_output filename)
+  SET(EXECUTABLE ./example_${filename})
+  message(STATUS "Generating example output files")
+  execute_process(COMMAND ${EXECUTABLE} OUTPUT_QUIET)
+  message(STATUS "Done generating example output files!")
+endfunction(generate_output)
+
+# Function for converting CSV files to VTK for example
+function(generate_visualization filename)
+  message(STATUS "Generating visualization files")
+  execute_process(COMMAND pvpython ${PARA_SRC_DIR}/visualize.py
+                  ${PARA_OUTPUT_DIR} ${filename} OUTPUT_QUIET)
+              message(STATUS "Done generating visualization files!")
+endfunction(generate_visualization)
+
 #############################
 # EXAMPLE 1: Collision
 #############################
 
 # Run example_collision
-execute_process(COMMAND ./example_collision OUTPUT_QUIET)
+generate_output(collision)
 # Generate visualization files for example_collision
-execute_process(COMMAND pvpython ${PARA_SRC_DIR}/visualize.py
-                ${PARA_OUTPUT_DIR} collision OUTPUT_QUIET)
+generate_visualization(collision)
 
 
 #############################

983420a822540334446399bbdd72c268e3c57d07
uvaaland <ubvaaland@gmail.com>
2017-01-10 15:11:20 -0500

    Collision example works; Output filenames now vary

--- example/example.cmake
+++ example/example.cmake
@@ -3,9 +3,10 @@
 #############################
 
 # Run example_collision
-execute_process(COMMAND ./example_collision)
+execute_process(COMMAND ./example_collision OUTPUT_QUIET)
 # Generate visualization files for example_collision
-execute_process(COMMAND pvpython ${PARA_SRC_DIR}/visualize.py ${PARA_OUTPUT_DIR})
+execute_process(COMMAND pvpython ${PARA_SRC_DIR}/visualize.py
+                ${PARA_OUTPUT_DIR} collision OUTPUT_QUIET)
 
 
 #############################

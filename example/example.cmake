d1de59d3be2e64be0489cb069c772b2d84e638f4
uvaaland <ubvaaland@gmail.com>
2017-01-10 14:30:55 -0500

    Primitive collision example working

--- example/example.cmake
+++ example/example.cmake
@@ -1,1 +1,20 @@
-execute_process(COMMAND ./example)
+#############################
+# EXAMPLE 1: Collision
+#############################
+
+# Run example_collision
+execute_process(COMMAND ./example_collision)
+# Generate visualization files for example_collision
+execute_process(COMMAND pvpython ${PARA_SRC_DIR}/visualize.py ${PARA_OUTPUT_DIR})
+
+
+#############################
+# EXAMPLE 2: Gravity
+#############################
+
+# Run example_collision
+#execute_process(COMMAND ./example_collision)
+# Generate visualization files for example_collision
+#execute_process(COMMAND pvpython ${PARA_SRC_DIR}/visualize.py ${PARA_OUTPUT_DIR})
+
+#############################

1fa8c5452d9a7b289dc3ef06d2813d5d325e878f
uvaaland <ubvaaland@gmail.com>
2017-01-10 17:39:25 -0500

    Add status reports to example generation

--- example/example.cmake
+++ example/example.cmake
@@ -1,23 +1,33 @@
-# Function for running the example script
+# Run the example script
 function(generate_output filename)
   SET(EXECUTABLE ./example_${filename})
-  message(STATUS "Generating example output files")
+  message(STATUS "Generating output files for the example")
   execute_process(COMMAND ${EXECUTABLE} OUTPUT_QUIET)
-  message(STATUS "Done generating example output files!")
+  message(STATUS "Done generating output files!")
 endfunction(generate_output)
 
-# Function for converting CSV files to VTK for example
+# Convert CSV files to VTK for example
 function(generate_visualization filename)
-  message(STATUS "Generating visualization files")
+  message(STATUS "Generating visualization files for the example")
   execute_process(COMMAND pvpython ${PARA_SRC_DIR}/visualize.py
                   ${PARA_OUTPUT_DIR} ${filename} OUTPUT_QUIET)
-              message(STATUS "Done generating visualization files!")
+  message(STATUS "Done generating visualization files!")
 endfunction(generate_visualization)
+
+# Print header
+function(print_header title number)
+  message(STATUS "")
+  message(STATUS "###########################")
+  message(STATUS "EXAMPLE ${number}: ${title}")
+  message(STATUS "###########################")
+  message(STATUS "")
+endfunction(print_header)
 
 #############################
 # EXAMPLE 1: Collision
 #############################
 
+print_header(Collision 1)
 # Run example_collision
 generate_output(collision)
 # Generate visualization files for example_collision
@@ -28,6 +38,7 @@ generate_visualization(collision)
 # EXAMPLE 2: Gravity
 #############################
 
+print_header(Gravity 2)
 # Run example_collision
 #execute_process(COMMAND ./example_collision)
 # Generate visualization files for example_collision

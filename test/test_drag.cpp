c0faf3aaf1b793bcd6890edbb4e03ace73fe0b21
buralcan <buralcan@princeton.edu>
2017-01-13 12:42:04 -0800

    Update test_drag.cpp

--- test/test_drag.cpp
+++ test/test_drag.cpp
@@ -7,72 +7,63 @@
 TEST(DragTest, directionTest) {
   int nparticles = 2;
   std::string filename =
-      "/home/uvaaland/jenkins/workspace/uvaaland/jenkins_ExMD/test/data_directionTest.txt";
-  Particles *particles;
-  particles = new Particles(filename, 0);
+      "/home/uvaaland/jenkins/workspace/uvaaland/jenkins_ExMD/test/"
+      "data_directionTest.txt";
+  Particles *particles = new Particles(filename, 0);
 
-    Distance *distance = new Distance(particles);
+  Distance *distance = new Distance(particles);
 
-    double gamma = 2;  // drag coefficient
+  double gamma = 2;  // drag coefficient
 
-    Force *drag = new Drag(gamma);
+  Force *drag = new Drag(gamma);
 
-    double forces[2][3] = {{0, 0, 0}, {0, 0, 0}};
+  double forces[2][3] = {{0, 0, 0}, {0, 0, 0}};
 
-    drag->ComputeForce(*particles, *distance, forces);
+  drag->ComputeForce(*particles, *distance, forces);
 
-    EXPECT_EQ(forces[0][0], -gamma*velocities[0][0]);
+  EXPECT_EQ(forces[0][0], -gamma * velocities[0][0]);
 }
-
 
 TEST(DragTest, directionTest3D) {
-    int nparticles = 2;
-    double positions[2][3] = {{0, 0, 0}, {0, 3, 0}};
-    double velocities[2][3] = {{10, 8, 6}, {0, 0, 0}};
-    double masses[2] = {1, 1};
-    double radii[2] = {1, 1};
+  int nparticles = 2;
+  std::string filename =
+      "/home/uvaaland/jenkins/workspace/uvaaland/jenkins_ExMD/test/"
+      "data_directionTest3D.txt";
+  Particles *particles = new Particles(filename, 0);
 
-    Particles *particles = new Particles(nparticles, positions, velocities,
-            masses, radii);
+  Distance *distance = new Distance(particles);
 
-    Distance *distance = new Distance(particles);
+  double gamma = 2;  // drag coefficient
 
-    double gamma = 2;  // drag coefficient
+  Force *drag = new Drag(gamma);
 
-    Force *drag = new Drag(gamma);
+  double forces[2][3] = {{0, 0, 0}, {0, 0, 0}};
 
-    double forces[2][3] = {{0, 0, 0}, {0, 0, 0}};
+  drag->ComputeForce(*particles, *distance, forces);
 
-    drag->ComputeForce(*particles, *distance, forces);
-
-    EXPECT_EQ(forces[0][0], -gamma*velocities[0][0]);
-    EXPECT_EQ(forces[0][1], -gamma*velocities[0][1]);
-    EXPECT_EQ(forces[0][2], -gamma*velocities[0][2]);
+  EXPECT_EQ(forces[0][0], -gamma * velocities[0][0]);
+  EXPECT_EQ(forces[0][1], -gamma * velocities[0][1]);
+  EXPECT_EQ(forces[0][2], -gamma * velocities[0][2]);
 }
-
 
 TEST(DragTest, zeroGamma) {
-    int nparticles = 2;
-    double positions[2][3] = {{0, 0, 0}, {0, 3, 0}};
-    double velocities[2][3] = {{10, 8, 6}, {0, 0, 0}};
-    double masses[2] = {1, 1};
-    double radii[2] = {1, 1};
+  int nparticles = 2;
+  std::string filename =
+      "/home/uvaaland/jenkins/workspace/uvaaland/jenkins_ExMD/test/"
+      "data_directionTest3D.txt";
+  Particles *particles = new Particles(filename, 0);
 
-    Particles *particles = new Particles(nparticles, positions, velocities,
-            masses, radii);
+  Distance *distance = new Distance(particles);
 
-    Distance *distance = new Distance(particles);
+  double gamma = 0;  // drag coefficient
 
-    double gamma = 0;  // drag coefficient
+  Force *drag = new Drag(gamma);
 
-    Force *drag = new Drag(gamma);
+  double forces[2][3] = {{0, 0, 0}, {0, 0, 0}};
 
-    double forces[2][3] = {{0, 0, 0}, {0, 0, 0}};
+  drag->ComputeForce(*particles, *distance, forces);
 
-    drag->ComputeForce(*particles, *distance, forces);
-
-    EXPECT_EQ(forces[0][0], 0);
-    EXPECT_EQ(forces[0][1], 0);
-    EXPECT_EQ(forces[0][2], 0);
+  EXPECT_EQ(forces[0][0], 0);
+  EXPECT_EQ(forces[0][1], 0);
+  EXPECT_EQ(forces[0][2], 0);
 }
-

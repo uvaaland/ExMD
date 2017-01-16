9ce6d92706c8cbb48a96d9280eddaf48c98200ee
Adrian Tasistro-Hart <adrian@tasistro.com>
2017-01-16 12:48:57 -0500

    compute accelerations without forces added now just returns zero accelerations

--- src/physics.cpp
+++ src/physics.cpp
@@ -177,13 +177,15 @@ int Physics::ComputeCollisions(Particles &particles,  \
 
 void Physics::ComputeAccelerations(Particles &particles, \
   Distance const &distances, double (*accelerations)[3]) {
-  // if no forces have been added to physics, give error
-  assert(static_cast<int>(forces_.size()) > 0);
   // make sure acclerations begin as zero
   for (int i = 0; i < particles.nparticles; i++) {
     for (int j = 0; j < 3; j++) {
       accelerations[i][j] = 0.;
     }
+  }
+  // if no forces have been added to physics, leave accelerations as zero
+  if (static_cast<int>(forces_.size()) == 0) {
+    return;
   }
   // create temporary array for general case of arbitrarily many forces
   double tmp_accelerations[particles.nparticles][3];
